e1c6a8d21b30e17b36a0b7f067f18210f4c45802
Wailo0 <iymashev@gmail.com>
2022-12-02 10:08:02 +0300

    Initial commit

---
+++ KataAcademy/src/Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class Main {
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int [] firstMass = new int [10];
+        int [] secondMass = new int [5];
+        int [] thirdMass = new int [5];
+        for (int i = 0; i < secondMass.length; i++) {
+            secondMass[i] = i;
+            thirdMass[i] = i*13;
+        }
+        for (int a : secondMass){
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
