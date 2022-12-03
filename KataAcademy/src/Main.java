2498146a155036f49394ba1ec5307ff6f464f18d
Wailo0 <iymashev@gmail.com>
2022-12-03 10:30:33 +0300

    massWork

--- KataAcademy/src/Main.java
+++ KataAcademy/src/Main.java
@@ -1,43 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.util.Arrays;
 
 
 public class Main {
 
 
-
-
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int [] firstMass = new int [10];
-        int [] secondMass = new int [5];
-        int [] thirdMass = new int [5];
-        for (int i = 0; i < secondMass.length; i++) {
-            secondMass[i] = i;
-            thirdMass[i] = i*13;
-        }
-        System.out.println("1");
-        System.out.println("1");
-        System.out.println("1");
-        for (int a : secondMass){
-
-        }
-
-
-
-
-
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(printOddNumbers(new int[]{1, 2, 3}, new int[]{4, 5, 6})));
     }
 
-
-
-
-
-
-
-
-
+    public static int[] printOddNumbers(int[] arr, int[] arr1) {
+        int j = 0, k = 0, i = 0;
+        int[] finalArr = new int[arr.length + arr1.length];
+        while (arr.length > j && arr1.length > k) {
+            finalArr[i++] = arr[j] <= arr1[k] ? arr[j++] : arr1[k++];
+        }
+        while (j < arr.length) {
+            finalArr[i++] = arr[j++];
+        }
+        while (k < arr1.length) {
+            finalArr[i++] = arr1[k++];
+        }
+        return finalArr;
+    }
 }
