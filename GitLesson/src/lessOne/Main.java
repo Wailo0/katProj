bf5e81cbf598e7e5ebd8c770b380a9d50e729612
Wailo0 <iymashev@gmail.com>
2022-12-29 11:32:18 +0300

    cm3

--- GitLesson/src/lessOne/Main.java
+++ GitLesson/src/lessOne/Main.java
@@ -6,17 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
-public static void main(String[] args) {
-    User user = new User(
-            "Alex", "Zaradin", 17
-    );
-    User user1 = new User(
-            "Stepan", "Sorokin", 21
-    );
-
-    List<User> userBin = new LinkedList<>();
-    userBin.add(user);
-    userBin.add(user1);
-    userBin.forEach(System.out::println);
-}
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
